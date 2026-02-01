04c2c04d08d27e249ded9e24199ba58fdf60c661
Hien Tran <hienthuytran@usf.edu>
2026-02-01 00:38:56 -0500

    First commit

---
+++ Product_pricing_assistant.js
@@ -0,0 +1,19 @@
+let productName = "Artbook";
+let costPerUnit = 15;
+let basePrice = 50;
+let discountRate = 0.23;
+let salesTaxRate = 0.075;
+let fixedMonthlyCosts = 800;
+let discountedPrice = basePrice * (1 - discountRate);
+let finalPriceWithTax = discountedPrice * (1 + salesTaxRate);
+let profitPerUnit = finalPriceWithTax - costPerUnit;
+let breakEvenUnits = Math.ceil(fixedMonthlyCosts / profitPerUnit);
+let isProfitablePerUnit = profitPerUnit > 0;
+
+console.log(" - Financial Report - ");
+console.log(`Product name: ${productName}`);
+console.log(`Discounted price (before tax): $${discountedPrice.toFixed(2)}`);
+console.log(`Final price: $${finalPriceWithTax.toFixed(2)}`);
+console.log(`Profit per unit: $${profitPerUnit.toFixed(2)}`);
+console.log(`Break even units: ${breakEvenUnits}`);
+console.log(`Is it profitable? ${isProfitablePerUnit}`);
---
+++ README.md
@@ -0,0 +1,2 @@
+Coding Challenge 02a: Product Pricing Assistant
+In this challenge, I'll build a simple product pricing assistant using JavaScript.
