d639cf402cbca6f32d568f15ca12b2ba833cf130
Hien Tran <hienthuytran@usf.edu>
2026-02-01 00:46:01 -0500

    Update README.md, js file with comments

--- Product_pricing_assistant.js
+++ Product_pricing_assistant.js
@@ -1,15 +1,19 @@
+// Initialize variables
 let productName = "Artbook";
 let costPerUnit = 15;
 let basePrice = 50;
 let discountRate = 0.23;
 let salesTaxRate = 0.075;
 let fixedMonthlyCosts = 800;
+
+// Calculate pricing and profit metrics
 let discountedPrice = basePrice * (1 - discountRate);
 let finalPriceWithTax = discountedPrice * (1 + salesTaxRate);
 let profitPerUnit = finalPriceWithTax - costPerUnit;
 let breakEvenUnits = Math.ceil(fixedMonthlyCosts / profitPerUnit);
 let isProfitablePerUnit = profitPerUnit > 0;
 
+// Print to console
 console.log(" - Financial Report - ");
 console.log(`Product name: ${productName}`);
 console.log(`Discounted price (before tax): $${discountedPrice.toFixed(2)}`);
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-Coding Challenge 02a: Product Pricing Assistant.
+ISM 3232 Coding Challenge 02a: Product Pricing Assistant.
 In this challenge, I’ll build a simple product pricing assistant using JavaScript.
